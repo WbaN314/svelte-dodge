8afc3d9a2afe90306563b9afbb2cb4d10f09e6d0
Sascha Stoll <sascha.stoll@sap.com>
2022-10-21 15:52:10 +0200

    More nice demo

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import Dodge from '$lib/Dodge.svelte';
 
-	let username: String = "";
-	let password: String = "";
-	let warning = false;
+	let username: String = '';
+	let password: String = '';
+	let warning = 0;
 	let login = false;
 
 	let timeout: NodeJS.Timeout;
@@ -18,21 +18,26 @@
 		<div>
 			Password:
 			{#if warning}
-				<span class="warning">Enter your password</span>
+				<span class={`warning ${warning >= 2 ? 'shakeY' : ''}`}>Enter your password</span>
 			{/if}
 		</div>
-		<input type="password" bind:value={password} on:input={() => warning = false} />
+		<input type="password" bind:value={password} on:input={() => (warning = 0)} />
 	</div>
 	<div>
 		<Dodge
-			box = {{up: 0, left: 0, right: 600, down: 500}}
+			box={{ up: 0, left: 0, right: 600, down: 0 }}
 			dodge={!(username?.length > 0 && password?.length > 0)}
 			on:move={() => {
 				clearTimeout(timeout);
-				warning = true;
+				warning += 1;
 				timeout = setTimeout(() => {
-					warning = false;
+					warning = 0;
 				}, 5000);
+			}}
+			on:transitionend={() => {
+				if (warning >= 2) {
+					warning = 1;
+				}
 			}}
 		>
 			<button
@@ -61,10 +66,38 @@
 	}
 
 	.warning {
+		display: inline-block;
 		color: red;
 	}
 
 	.success {
 		color: green;
+	}
+
+	@keyframes shakeY {
+		from,
+		to {
+			transform: translate3d(0, 0, 0);
+		}
+
+		10%,
+		30%,
+		50%,
+		70%,
+		90% {
+			transform: translate3d(0, -10px, 0);
+		}
+
+		20%,
+		40%,
+		60%,
+		80% {
+			transform: translate3d(0, 10px, 0);
+		}
+	}
+
+	.shakeY {
+		animation-name: shakeY;
+		animation-duration: 1s;
 	}
 </style>
